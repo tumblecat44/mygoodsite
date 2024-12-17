7178fba823a65d53c9f620e1c6bd3579bab02633
dlrjsgml44 <leegeh1213@gmail.com>
2024-12-17 20:38:43 +0900

    add : my info

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>대구소프트웨어마이스터고등학교 이건희</p>
+      <img src="" alt="" />
     </div>
   );
 }
